e1ac0c0c1cb9bf890e183343babba4eab0687e6a
tmkkz <kinaba@tmksolutions.com>
2021-11-27 12:03:05 -0800

    impliment simple html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const component = () => {
+  const element = document.createElement('div')
+  const array = ['Hellow', 'webpack'];
+  element = _.join(array, ' ');
+  return element;
+}
+
+document.body.appendChild(component());
