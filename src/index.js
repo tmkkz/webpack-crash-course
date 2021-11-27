78fa0c47c775cffd1f324bf07247388a806d3ee9
tmkkz <kinaba@tmksolutions.com>
2021-11-27 14:52:50 -0800

    webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import _ from 'lodash';
-
-const component = () => {
-  const element = document.createElement('div')
-  const array = ['Hellow', 'webpack'];
-  element = _.join(array, ' ');
-  return element;
-}
-
-document.body.appendChild(component());
+console.log("Hello World!");
