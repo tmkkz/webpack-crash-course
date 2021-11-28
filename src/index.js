a59b5d516dca0cb14e9c86a7086042277901ca94
tmkkz <kinaba@tmksolutions.com>
2021-11-27 17:54:11 -0800

    url-loader, file-loader gone in webpack5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "./style.css";
+import yaruo from "./yaruo.png";
 
 console.log("Hello World!");
 
 document.body.classList.add('haikei');
+
+const image = new Image();
+image.src = yaruo;
+document.body.appendChild(image);
